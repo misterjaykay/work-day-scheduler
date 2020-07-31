1afb939c8e99dbfaa0782436b57824b7e3380550
Jonghyun Kang <misterjaykay@gmail.com>
2020-07-30 23:08:15 -0400

    testing with time,textarea,button

--- how-to/README.md
+++ how-to/README.md
@@ -17,8 +17,8 @@ SO THAT I can manage my time effectively
 ```
 GIVEN I am using a daily planner to create a schedule
 
-<!-- WHEN I open the planner
-THEN the current day is displayed at the top of the calendar -->
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
 
 WHEN I scroll down
 THEN I am presented with timeblocks for standard business hours
--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+
+
 //////// MOMENT.JS USED ///////////
 var createH3 = $("<h3>");
 $("#currentDay").append(createH3)
@@ -16,47 +20,48 @@ setInterval(update, 1000);
 ///////////////////////////////////
 
 
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
 
 
-var nineAm = $("<section>")
-nineAm.text("This is nine o clock");
-$(".container").append(nineAm);
 
-var tenAm = $("<section>")
-tenAm.text("This is ten o clock");
-$(".container").append(tenAm);
+////////////// BUSINESS HOURS //////////
 
+// var nineHour = $("<div>");
+// nineHour.text("This is nine o clock");
+// nineHour.attr("style", "border-style:solid;")
+// $(".hour").append(nineHour);
+// var nineType = $("<input>");
+// nineType.text("This is nine text");
+// nineType.attr("class", "input");
+// $(".schedule").append(nineType);
 
-var elevenAm = $("<section>")
-elevenAm.text("This is eleven o clock");
-$(".container").append(elevenAm);
+var tenHour = $("<div>");
+var elevenHour = $("<div>");
+var twelveHour = $("<div>");
+var oneHour = $("<div>");
+var twoHour = $("<div>");
+var threeHour = $("<div>");
+var fourHour = $("<div>");
+var fiveHour = $("<div>");
 
+///////////////////////////////
 
-var twelveAm = $("<section>")
-twelveAm.text("This is twleve o clock");
-$(".container").append(twelveAm);
-
-
-var onePm = $("<section>")
-onePm.text("This is one o clock");
-$(".container").append(onePm);
-
-
-var twoPm = $("<section>")
-twoPm.text("This is two o clock");
-$(".container").append(twoPm);
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
 
-var threePm = $("<section>")
-threePm.text("This is three o clock");
-$(".container").append(threePm);
+// WHEN I click into a timeblock
+// THEN I can enter an event
 
 
-var fourPm = $("<section>")
-fourPm.text("This is four o clock");
-$(".container").append(fourPm);
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
 
 
-var fivePm = $("<section>")
-fivePm.text("This is five o clock");
-$(".container").append(fivePm);
+// WHEN I refresh the page
+// THEN the saved events persist
+
+
+
+
