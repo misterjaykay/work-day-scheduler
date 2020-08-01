72ce7209c542288c4ff4ccff1fb1b2ee4ca780f6
Jonghyun Kang <misterjaykay@gmail.com>
2020-08-01 19:21:02 -0400

    rewrite button function to react with each button clicks

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-
-
 //////// MOMENT.JS FOR CURRENT TIME APPEARANCE
+var date = moment.utc().format('YYYY-MM-DD HH:mm:ss');
+var localTime  = moment.utc(date).toDate();
+var currentTime = moment(localTime).format("MMMM Do YYYY, h:mm");
 var createH3 = $("<h3>");
-$("#currentDay").append(createH3)
+$(createH3).text(currentTime);
+$("#currentDay").append(createH3);
 
-var update = setInterval( function() {
-    var date = moment.utc().format('YYYY-MM-DD HH:mm:ss');
-    var localTime  = moment.utc(date).toDate();
-    var currentTime = moment(localTime).format("MMMM Do YYYY, h:mm");
-    createH3.text(currentTime);
-    // var createH3 = $("<h3>");
-    // createH3.text(currentTime);
-    // $("#currentDay").append(createH3)
-}, 1000)
-setInterval(update, 1000);
-
-
-
+///////// OPTION FOR YOU TO USE THE TIME TO BE UPDATED WITHIN INTERVAL
+// var update = setInterval( function() {
+//     var date = moment.utc().format('YYYY-MM-DD HH:mm:ss');
+//     var localTime  = moment.utc(date).toDate();
+//     var currentTime = moment(localTime).format("MMMM Do YYYY, h:mm");
+//     createH3.text(currentTime);
+//     // var createH3 = $("<h3>");
+//     // createH3.text(currentTime);
+//     // $("#currentDay").append(createH3)
+// }, 1000)
+// setInterval(update, 1000);
 
 
 
@@ -34,33 +32,68 @@ for (var i = 0; i < allHourList.length; i++) {
     var tBody = (".tbody");
     $(tBody).append(tRow);
     var timeDiv = $("<th>").text(allHourList[i]).attr("scope", "row").attr("style", "text-align: center;");
-    var messageDiv = $("<td>").html("<input class='textMessage-" + i + "' size='100' type='text' >")
-    var buttonDiv = $("<td>").html("<input data-value'" + i + "' type='button' value='submit' >").attr("style", "text-align: center;").addClass("submit-button-" + i);
+    var messageDiv = $("<td>").html("<input class='textMessage-" + i + "' size='100' type='text' data-input='" + i + "' >");
+    var buttonDiv = $("<td>").html("<input class='submit-button' data-value='" + i + "' type='button' value='submit' >").attr("style", "text-align: center;")
+    // AT BUTTONDIV, REMOVED BOTTOM LINE
+    // .addClass("submit-button-" + i);
     
     $(tRow).prepend(timeDiv, messageDiv, buttonDiv);
     $(tBody).append(tRow);
     
 }
 
-///////////// GET THE VALUE FROM INPUT
-// function getInputValue(){
-//     // Selecting the input element and get its value 
-//     var inputVal = document.getElementById("myInput").value;
-    
-//     // Displaying the value
-//     alert(inputVal);
-var submitButton = $(".submit-button-1");
 
-$(submitButton).on("click", function(event) {
+
+//////////// SUBMIT BUTTONS TO SAVE EACH TEXT TO LOCAL STORAGE
+$(".submit-button").on("click", function(event) {
     event.preventDefault();
-    var submitText = $(".textMessage-1").val().trim();
-    console.log(submitText);
-    console.log(typeof submitText);
+    var saveButton = $(this).attr("data-value");
     
-    alert("hi")
-    
-    localStorage.setItem("test", JSON.stringify(submitText));
-    alert("string")
+    if (saveButton == 0) {
+        var textRowOne = $(".textMessage-0").val().trim();
+        localStorage.setItem("textRowOne", JSON.stringify(textRowOne));
+    }
+
+    if (saveButton == 1) {
+        var textRowTwo = $(".textMessage-1").val().trim();
+        localStorage.setItem("textRowTwo", JSON.stringify(textRowTwo)); 
+    }
+
+    if (saveButton == 2) {
+        var textRowThree = $(".textMessage-2").val().trim();
+        localStorage.setItem("textRowThree", JSON.stringify(textRowThree));
+    }
+
+    if (saveButton == 3) {
+        var textRowFour = $(".textMessage-3").val().trim();
+        localStorage.setItem("textRowFour", JSON.stringify(textRowFour));
+    }
+
+    if (saveButton == 4) {
+        var textRowFive = $(".textMessage-4").val().trim();
+        localStorage.setItem("textRowFive", JSON.stringify(textRowFive));
+    }
+
+    if (saveButton == 5) {
+        var textRowSix = $(".textMessage-5").val().trim();
+        localStorage.setItem("textRowSix", JSON.stringify(textRowSix));
+    }
+
+    if (saveButton == 6) {
+        var textRowSeven = $(".textMessage-6").val().trim();
+        localStorage.setItem("textRowSeven", JSON.stringify(textRowSeven));
+    }
+
+    if (saveButton == 7) {
+        var textRowEight = $(".textMessage-7").val().trim();
+        localStorage.setItem("textRowEight", JSON.stringify(textRowEight));
+    }
+
+    if (saveButton == 8) {
+        var textRowNine = $(".textMessage-8").val().trim();
+        localStorage.setItem("textRowNine", JSON.stringify(textRowNine));
+    }
+
 });
 
 ///////// USING FUNCTION TO BRING BACK DATA
@@ -70,9 +103,40 @@ $(submitButton).on("click", function(event) {
 //     console.log(localText);
 // }
 
-var localText = JSON.parse(localStorage.getItem("test"));
-    $(".textMessage-1").attr("value", localText);
-    console.log(localText);
+function bringBack() {
+
+    if (localStorage.getItem("textRowOne") == null) {
+    var localTextOne = JSON.parse(localStorage.getItem("textRowOne"));
+        $(".textMessage-0").attr("textRowOne", localTextOne);
+        console.log(localTextOne);
+    }
+
+    var localTextTwo = JSON.parse(localStorage.getItem("textRowTwo"));
+        $(".textMessage-1").attr("textRowTwo", localTextTwo);
+
+    var localTextThree = JSON.parse(localStorage.getItem("textRowThree"));
+        $(".textMessage-2").attr("textRowThree", localTextThree);
+
+    var localTextFour = JSON.parse(localStorage.getItem("textRowFour"));    
+        $(".textMessage-3").attr("textRowFour", localTextFour);
+
+    var localTextFive = JSON.parse(localStorage.getItem("textRowFive"));
+        $(".textMessage-4").attr("textRowFive", localTextFive);
+
+    var localTextSix = JSON.parse(localStorage.getItem("textRowSix"));
+        $(".textMessage-5").attr("textRowSix", localTextSix);
+
+    var localTextSeven = JSON.parse(localStorage.getItem("textRowSeven"));
+        $(".textMessage-6").attr("textRowSeven", localTextSeven);
+
+    var localTextEight = JSON.parse(localStorage.getItem("textRowEight"));
+        $(".textMessage-7").attr("textRowEight", localTextEight);
+
+    var localTextNine = JSON.parse(localStorage.getItem("textRowNine"));
+        $(".textMessage-8").attr("textRowNine", localTextNine);
+
+        console.log(localText);
+}
 
 ///////// FUNCTION TO SAVE DATA INTO LOCALSTORAGE
 // function storeTodos() {
